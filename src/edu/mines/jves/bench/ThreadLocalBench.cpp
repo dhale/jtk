b01291ee43daaa23ad7e8b7674862f9d1fdec828
Dave Hale <dhale@mines.edu>
2004-11-09 23:46:32 +0000

    Renamed class, clean up docs and printout.

--- src/edu/mines/jves/bench/ThreadLocalBench.cpp
+++ src/edu/mines/jves/bench/ThreadLocalBench.cpp
@@ -2,14 +2,14 @@
 #include "jni.h"
 
 extern "C" JNIEXPORT void JNICALL
-Java_edu_mines_jves_bench_TlBench_foo(
+Java_edu_mines_jves_bench_ThreadLocalBench_nativeMethod(
   JNIEnv* env, jclass cls,
   jint i)
 {
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_edu_mines_jves_bench_TlBench_bar(
+Java_edu_mines_jves_bench_ThreadLocalBench_nativeMethodWithContext(
   JNIEnv* env, jclass cls,
   jlong pointer, jint i)
 {
---
+++ src/edu/mines/jves/bench/ThreadLocalBench.java
@@ -0,0 +1,68 @@
+package edu.mines.jves.bench;
+
+import edu.mines.jves.util.Stopwatch;
+
+/**
+ * Benchmark ThreadLocal with respect to static native methods.
+ * <p>
+ * For example, in OpenGL, we may need to pass a function pointer
+ * to a static native method. That function pointer is stored in an
+ * OpenGL context. In any thread, either zero or one OpenGL context
+ * is the current context. However, different threads can have
+ * different (current) OpenGL contexts. Therefore, when a context
+ * is made current in a thread, we set that context in a static 
+ * ThreadLocal object, which maps the current thread to the current 
+ * context. From that context, we can get the function pointer we
+ * must pass to the native method.
+ * <p>
+ * Here, we benchmark the overhead of the ThreadLocal stuff, and 
+ * compare with the overhead of a call to a trivial do-nothing native 
+ * method.
+ * @author Dave Hale
+ * @version 2004.11.08
+ */
+public class ThreadLocalBench {
+
+  public static void main(String[] args) {
+    double maxtime = 1.0;
+    Stopwatch sw = new Stopwatch();
+    int ncall,rate;
+
+
+    // Time calls to trivial native method.
+    System.out.println("Benchmarking static native method calls:");
+    sw.restart();
+    for (ncall=0; sw.time()<maxtime; ++ncall) {
+      nativeMethod(ncall);
+    }
+    sw.stop();
+    rate = (int)(ncall/sw.time());
+    System.out.println("  calls/sec = "+rate);
+
+    // Time calls to trivial native method with thread-specific context.
+    System.out.println("Benchmarking again with ThreadLocal context:");
+    Context context = new Context();
+    context.pointer = 314159;
+    contexts.set(context);
+    sw.restart();
+    for (ncall=0; sw.time()<maxtime; ++ncall) {
+      context = (Context)contexts.get();
+      nativeMethodWithContext(context.pointer,ncall);
+    }
+    sw.stop();
+    rate = (int)(ncall/sw.time());
+    System.out.println("  calls/sec = "+rate);
+  }
+
+  private static class Context {
+    long pointer;
+  }
+
+  private static ThreadLocal contexts = new ThreadLocal();
+  private static native void nativeMethod(int i);
+  private static native void nativeMethodWithContext(long pointer, int i);
+
+  static {
+    System.loadLibrary("edu_mines_jves_bench");
+  }
+}
