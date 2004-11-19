6a38f6ed701cd3031b9732ca9f080930871dd56d
Dave Hale <dhale@mines.edu>
2004-11-19 17:07:43 +0000

    Modified scripts.

--- bin/build.bat
+++ bin/build.bat
@@ -1,15 +1,14 @@
 @echo off
 setlocal
 
+@rem Customize these locations as necessary.
 set TOOLS=c:\tools
 set VC_HOME=%TOOLS%\vc\vctk2003
 set SDK_HOME=%TOOLS%\mssdk
-set JAVA_HOME=%TOOLS%\Java\j2sdk1.4.2_06
 set ANT_HOME=%TOOLS%\ant
+set JDK_HOME=%TOOLS%\Java\j2sdk1.4.2_06
 
-set JAVA_INCLUDE=%JAVA_HOME%\include
-
-set TPATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;%VC_HOME%\bin
+set TPATH=%JDK_HOME%\bin;%ANT_HOME%\bin;%VC_HOME%\bin
 set PATH=%TPATH%;%PATH%
 set LIB=%VC_HOME%\lib;%SDK_HOME%\lib
 
