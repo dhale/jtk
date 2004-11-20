f49f5e5244d236eb891985b3a7ae9d1a0dda35fa
Dave Hale <dhale@mines.edu>
2004-11-20 00:09:05 +0000

    New class for testing NIO buffers.

---
+++ src/edu/mines/jves/bench/BufferBench.java
@@ -0,0 +1,48 @@
+package edu.mines.jves.bench;
+
+import java.nio.*;
+
+/**
+ * Test NIO buffers.
+ * @author Dave Hale
+ * @version 2004.11.19
+ */
+public class BufferBench {
+
+  public static void main(String[] args) {
+    testOutOfMemory();
+  }
+
+  public static void testOutOfMemory() {
+    int capacity = 1000000000;
+    int nbuf = 10;
+    byte data = 31;
+    for (int ibuf=0; ibuf<nbuf; ++ibuf) {
+      ByteBuffer bb = newByteBuffer(capacity);
+      bb.put(capacity/2,data);
+      bb.put(capacity-1,data);
+      //for (int i=0; i<capacity; ++i)
+      //  bb.put(i,data);
+    }
+  }
+
+  private static ByteBuffer newByteBuffer(int capacity) {
+    ByteBuffer bb = null;
+    try {
+      System.out.println("allocating "+capacity+" bytes");
+      bb = ByteBuffer.allocateDirect(capacity).order(ByteOrder.nativeOrder());
+    } catch (OutOfMemoryError e1) {
+      System.gc();
+      System.out.println("attempted gc after"+e1.getMessage());
+      System.out.println("now attempting to allocate again");
+      try {
+        bb = ByteBuffer.allocateDirect(capacity);
+      } catch (OutOfMemoryError e2) {
+        System.out.println("failed allocate after gc"+e2.getMessage());
+      }
+    }
+    return bb;
+  }
+}
+
+
