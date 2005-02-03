845b3ff76037497a47508a78e8fc7603ef277490
Dave Hale <dhale@mines.edu>
2005-02-03 01:33:12 +0000

    Different test sequence.

--- src/edu/mines/jtk/mosaic/test/LollipopViewTest.java
+++ src/edu/mines/jtk/mosaic/test/LollipopViewTest.java
@@ -23,8 +23,7 @@ import edu.mines.jtk.mosaic.*;
 public class LollipopViewTest {
 
   public static void main(String[] args) {
-    //int nrow = 2;
-    int nrow = 1;
+    int nrow = 2;
     int ncol = 1;
     Set<Mosaic.AxesPlacement> axesPlacement = EnumSet.of(
       Mosaic.AxesPlacement.LEFT,
@@ -37,10 +36,18 @@ public class LollipopViewTest {
     mosaic.setPreferredSize(new Dimension(950,400));
 
     Tile tileA = mosaic.getTile(0,0);
-    //Tile tileB = mosaic.getTile(1,0);
+    Tile tileB = mosaic.getTile(1,0);
 
-    tileA.addTiledView(makeView(101, 1.0,-50.0,1.0));
-    //tileB.addTiledView(makeView( 13,-1.0,  5.0,2.0));
+    int n = 101;
+    double d = 1.0;
+    double f = -(n-1)/2.0;
+    float[] x = new float[n];
+    float[] y = new float[n];
+    x[0] = x[n-1] = x[n/2] = 1.0f;
+    filter(0.8f,x,y);
+
+    tileA.addTiledView(makeView(n,d,f,x));
+    tileB.addTiledView(makeView(n,d,f,y));
 
     TileZoomMode zoomMode = new TileZoomMode(mosaic);
 
@@ -65,17 +72,32 @@ public class LollipopViewTest {
   }
 
   private static LollipopView makeView(
-    int nx, double dx, double fx, double a) 
+    int nx, double dx, double fx, float[] f) 
   {
     Sampling sx = new Sampling(nx,dx,fx);
-    float[] f = new float[nx];
-    for (int ix=0; ix<nx; ++ix) {
-      double xi = fx+ix*dx;
-      f[ix] = (float)(a*cos(2.0*PI*xi*(1.0+0.02*xi)/20));
-    }
     LollipopView lv = new LollipopView(sx,f);
     lv.setLollipopColor(Color.BLUE);
     lv.setShowZero(LollipopView.ShowZero.MIDDLE);
     return lv;
+  }
+  private static void filter(float a, float[] x, float[] y) {
+    int n = x.length;
+    float yim1 = 0.0f;
+    for (int i=0; i<n; ++i) {
+      y[i] = a*yim1+x[i];
+      yim1 = y[i];
+    }
+    float yip1 = 0.0f;
+    for (int i=n-2; i>=0; --i) {
+      float yi = a*(yip1+x[i+1]);
+      y[i] += yi;
+      yip1 = yi;
+    }
+  }
+  private static void cosine(float a, int nx, double dx, double fx, float[] f) {
+    for (int ix=0; ix<nx; ++ix) {
+      double xi = fx+ix*dx;
+      f[ix] = (float)(a*cos(2.0*PI*xi*(1.0+0.02*xi)/20));
+    }
   }
 }
