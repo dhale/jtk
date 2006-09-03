040cc33cd6c00d146b3529cb147cb250dcdfaa46
Bill Harlan <bill.harlan@gmail.com>
2006-09-03 19:20:25 +0000

    Add all tests

--- src/edu/mines/jtk/opt/test/Suite.java
+++ src/edu/mines/jtk/opt/test/Suite.java
@@ -4,12 +4,6 @@ This program and accompanying materials are made available under the terms of
 the Common Public License - v1.0, which accompanies this distribution, and is
 available at http://www.eclipse.org/legal/cpl-v10.html
 ****************************************************************************/
-/****************************************************************************
-Copyright (c) 2005, Colorado School of Mines and others. All rights reserved.
-This program and accompanying materials are made available under the terms of
-the Common Public License - v1.0, which accompanies this distribution, and is
-available at http://www.eclipse.org/legal/cpl-v10.html
-****************************************************************************/
 package edu.mines.jtk.opt.test;
 
 import junit.framework.Test;
@@ -18,7 +12,6 @@ import junit.framework.TestSuite;
 /**
  * Tests all classes in package edu.mines.jtk.opt.
  * @author W.S. Harlan
- * @version 2005.03.24
  */
 public class Suite extends TestSuite {
 
@@ -28,14 +21,20 @@ public class Suite extends TestSuite {
   public static Test suite() {
     TestSuite suite = new TestSuite();
 
+    suite.addTestSuite(ArrayVect1fsTest.class);
+    suite.addTestSuite(ArrayVect1fTest.class);
     suite.addTestSuite(ArrayVect1Test.class);
+    suite.addTestSuite(ArrayVect2fTest.class);
+    suite.addTestSuite(ArrayVect2Test.class);
+    suite.addTestSuite(ArrayVect3fTest.class);
     suite.addTestSuite(BrentMinFinderTest.class);
     suite.addTestSuite(BrentZeroFinderTest.class);
     suite.addTestSuite(CoordinateTransformTest.class);
     suite.addTestSuite(GaussNewtonSolverTest.class);
+    suite.addTestSuite(LineSearchTest.class);
     suite.addTestSuite(QuadraticSolverTest.class);
     suite.addTestSuite(ScalarSolverTest.class);
-
+    suite.addTestSuite(ScalarVectTest.class);
     return suite;
   }
 
