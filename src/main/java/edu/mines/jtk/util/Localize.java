3e27836bb298278345e15dcdd270e9c52d169d02
Bill Harlan <bharlan@lgc.com>
2012-07-25 15:22:21 -0600

    Added convenience methods for localization using a Formatter pattern and localized resource bundles.

--- src/main/java/edu/mines/jtk/util/Localize.java
+++ src/main/java/edu/mines/jtk/util/Localize.java
@@ -1,63 +1,234 @@
 /****************************************************************************
-Copyright (c) 2003, Landmark Graphics and others. All rights reserved.
-This program and accompanying materials are made available under the terms of
-the Common Public License - v1.0, which accompanies this distribution, and is
-available at http://www.eclipse.org/legal/cpl-v10.html
-****************************************************************************/
+  Copyright (c) 2003, Landmark Graphics and others. All rights reserved.
+  This program and accompanying materials are made available under the terms of
+  the Common Public License - v1.0, which accompanies this distribution, and is
+  available at http://www.eclipse.org/legal/cpl-v10.html
+ ****************************************************************************/
 package edu.mines.jtk.util;
 
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.regex.*;
 
-/** Filter a string with specified ResourceBundles.
-    @author W.S. Harlan, Landmark Graphics
-*/
+/**
+ * Localize messages for end users, using a Formatter pattern and a localized resource bundle.
+ * <p/>
+ * 
+ * <pre>
+ * In a class like MyPanel.java you might have lines like this:
+ * 
+ * double value = 3.14;
+ * String msg = "The value "+value+" is too large";
+ * 
+ * Instead, in the same package as MyPanel.java, create a file
+ * MyPanel.properties, with this line:
+ * 
+ * too_large = The value %g is too large.
+ * 
+ * Then you can replace the original lines by this.
+ * 
+ * private static Localize local = new Localize(MyPanel.class);
+ * ...
+ * 
+ * double value = 3.14;
+ * String msg = local.format("too_large", value);
+ * 
+ * If you create an additional file called MyPanel_es.properties, then
+ * that file will be used automatically in Spanish-speaking locales.
+ * 
+ * @author W.S. Harlan, Landmark Graphics
+ */
 public class Localize {
-  private static final Logger LOG = Logger.getLogger(Localize.class.getName());
-  private static final Pattern s_tokens = Pattern.compile("[$][{](.+?)[}]");
+    private static final Logger LOG = Logger.getLogger(Localize.class.getName());
+    private static final Pattern s_tokens = Pattern.compile("[$][{](.+?)[}]");
 
-  /** Filter the specified string with the specified resource bundle.
+    private static final Map<String, ResourceBundle> s_rb = new HashMap<String, ResourceBundle>();
+
+    private final ResourceBundle resourceBundle;
+    private final Locale locale;
+    private final Class<?> clazz;
+    private final String resourceBundleName;
+
+    /**
+     * Construct for localized messages.
+     * 
+     * @param clazz Name of client class that requires localization Class<Foo> is assumed to have property files
+     *        Foo.properties, Foo_es.properties, etc. Uses default Locale.
+     */
+    public Localize(final Class<?> clazz) {
+        this(clazz, null);
+    }
+
+    /**
+     * Construct for localized messages.
+     * 
+     * @param clazz Name of client class that requires localization
+     * @param resourceBundleName Name of ResourceBundle to be used for localizing messages. If null, then Class<Foo> is
+     *        assumed to have property files Foo.properties, Foo_es.properties, etc. Uses default Locale.
+     */
+    public Localize(final Class<?> clazz, final String resourceBundleName) {
+        this(clazz, resourceBundleName, null);
+    }
+
+    /**
+     * Construct for localized messages.
+     * 
+     * @param clazz Name of client class that requires localization
+     * @param resourceBundleName Name of ResourceBundle to be used for localizing messages. If null, then Class<Foo> is
+     *        assumed to have property files Foo.properties, Foo_es.properties, etc.
+     * @param locale Locale to use for localization. If null, then will use default Locale.
+     */
+    public Localize(final Class<?> clazz, final String resourceBundleName, final Locale locale) {
+        this.clazz = clazz;
+        this.resourceBundleName = resourceBundleName;
+        resourceBundle = getResourceBundle(clazz, resourceBundleName, locale);
+        this.locale = locale;
+    }
+
+    /**
+     * Format a localized message, with java.util.Formatter and the appropriate resource.
+     * 
+     * @param key used to specify format string in properties file. If not found, then will be used as the format.
+     * @param args Optional arguments to be passed to Formatter.format method.
+     * @return formatted localized String
+     */
+    public String format(final String key, final Object... args) {
+        if (key == null) {
+            return null;
+        }
+        String format;
+        try {
+            if (resourceBundle != null) {
+                format = resourceBundle.getString(key);
+            } else {
+                format = key;
+            }
+        } catch (final MissingResourceException mre) {
+            format = key;
+        }
+        final Formatter formatter = new Formatter(locale);
+        return formatter.format(format, args).out().toString().trim();
+    }
+
+    /**
+     * Get a resource bundle associated with a class.
+     * 
+     * @param clazz Name of client class that requires resource bundle.
+     * @param resourceBundleName Name of ResourceBundle to be used for localizing messages. If null, then Class<Foo> is
+     *        assumed to have property files Foo.properties, Foo_es.properties, etc.
+     * @param locale Locale to use for localization. If null, then will use default Locale.
+     * @return Get ResourceBundle for this locale.
+     */
+    public static ResourceBundle getResourceBundle(final Class<?> clazz, String resourceBundleName, Locale locale) {
+
+        if (locale == null) {
+            locale = Locale.getDefault();
+        }
+        final String key = clazz.getName() + ";" + resourceBundleName + ";" + locale.toString();
+        synchronized (s_rb) {
+
+            ResourceBundle resourceBundle = s_rb.get(key);
+            if (resourceBundle == null) {
+                if (resourceBundleName == null) {
+                    String cn = clazz.getName();
+                    if (cn == null) {
+                        final Class<?> c2 = clazz.getEnclosingClass();
+                        if (c2 != null) {
+                            cn = c2.getName();
+                        }
+                    }
+                    if (cn == null) {
+                        throw new IllegalArgumentException("Specify top-level class.  "
+                                + "This class does not have a canonical name: " + clazz.getName());
+                    }
+                    resourceBundleName = cn;
+                } else {
+                    resourceBundleName = clazz.getPackage().getName() + "." + resourceBundleName;
+                }
+                try { // see if throws MissingResourceException
+                    resourceBundle = ResourceBundle.getBundle(resourceBundleName, locale, clazz.getClassLoader());
+                    s_rb.put(key, resourceBundle);
+                } catch (final MissingResourceException e) {
+                    // No resource file provided. Plan to return null.
+                }
+            }
+            return s_rb.get(key);
+        }
+    }
+
+    /**
+     * Get the best localized message from a Throwable that may contain other Throwables as a cause.
+     * 
+     * @param throwable a Throwable that may contain other Throwables as a cause.
+     * @return best localized message, unwrapping as necessary.
+     */
+    public static String getMessage(final Throwable throwable) {
+        if (throwable.getCause() == null) {
+            final String localizedMessage = throwable.getLocalizedMessage();
+            if (localizedMessage != null) {
+                return localizedMessage;
+            }
+            final String message = throwable.getMessage();
+            if (message != null) {
+                return message;
+            }
+            return throwable.toString();
+        }
+        final String causeToString = throwable.getCause().toString();
+        final String localized = throwable.getLocalizedMessage();
+        if (localized == null || localized.equals(causeToString)) {
+            return getMessage(throwable.getCause());
+        }
+        return localized;
+    }
+
+    @Override
+    public String toString() {
+        return "Localize{" + ", locale=" + locale + ", clazz=" + clazz + ", resourceBundleName='" + resourceBundleName
+                + '\'' + '}';
+    }
+
+    /** Filter the specified string with the specified resource bundle.
       First the entire string is filtered, then substrings
       delimited as ${key} are filtered.
       @param message Filter this string
       @param catalog Get keys and values from this.
       @return Filtered string.
-   */
-  public static String filter(String message, ResourceBundle catalog) {
-    if (catalog == null)
-      return message;
+     */
+    public static String filter(String message, ResourceBundle catalog) {
+        if (catalog == null)
+            return message;
 
-    // see if entire string is a key
-    try {
-      message = catalog.getString(message);
-    } catch (MissingResourceException ex) {/*no substitution*/}
+        // see if entire string is a key
+        try {
+            message = catalog.getString(message);
+        } catch (MissingResourceException ex) {/*no substitution*/}
 
-    // see if substrings are keys
-    Matcher matcher = s_tokens.matcher(message);
-    int numberMatches = 0;
-    while (matcher.find()) {++numberMatches;}
+        // see if substrings are keys
+        Matcher matcher = s_tokens.matcher(message);
+        int numberMatches = 0;
+        while (matcher.find()) {++numberMatches;}
 
-    // substitute from end backwards
-    for (int match=numberMatches; match>0; --match) {
-      matcher.reset();
-      for (int i=0; i<match; ++i) {
-        matcher.find();
-      }
-      MatchResult mr = matcher.toMatchResult();
-      try {
-        String key = mr.group(1);
-        String replacement = catalog.getString(key);
-        int start = mr.start();
-        int end = mr.end();
-        message = message.substring(0,start) + replacement
-          + message.substring(end);
-      } catch (MissingResourceException ex) {/* do not substitute */}
+        // substitute from end backwards
+        for (int match=numberMatches; match>0; --match) {
+            matcher.reset();
+            for (int i=0; i<match; ++i) {
+                matcher.find();
+            }
+            MatchResult mr = matcher.toMatchResult();
+            try {
+                String key = mr.group(1);
+                String replacement = catalog.getString(key);
+                int start = mr.start();
+                int end = mr.end();
+                message = message.substring(0,start) + replacement
+                    + message.substring(end);
+            } catch (MissingResourceException ex) {/* do not substitute */}
+        }
+        return message;
     }
-    return message;
-  }
 
-  /** Filter the specified string with a ResourceBundle
+    /** Filter the specified string with a ResourceBundle
       for the specified class.
       First the entire string is filtered, then substrings
       delimited as ${key} are filtered.
@@ -69,67 +240,69 @@ public class Localize {
       directory as the compiled Foo.class.
       Test with java -Duser.language=ch
       @return Filtered string.
-   */
-  public static String filter(String message, Class<?> resourceClass) {
-    ClassLoader cl = resourceClass.getClassLoader();
-    if (cl == null) {
-      LOG.warning("Could not get ClassLoader from "+resourceClass.getName());
-      cl = ClassLoader.getSystemClassLoader();
+      @deprecated Prefer other methods for standard localization.
+     */
+    @Deprecated
+    public static String filter(String message, Class<?> resourceClass) {
+        ClassLoader cl = resourceClass.getClassLoader();
+        if (cl == null) {
+            LOG.warning("Could not get ClassLoader from "+resourceClass.getName());
+            cl = ClassLoader.getSystemClassLoader();
+        }
+        Locale currentLocale = Locale.getDefault();
+        String name = resourceClass.getName();
+        ResourceBundle catalog =
+            ResourceBundle.getBundle(name, currentLocale, cl);
+        if (catalog == null) {
+            LOG.warning("Could not get ResourceBundle "+name+" for "+
+                    currentLocale+" from "+cl);
+        }
+        return filter(message, catalog);
     }
-    Locale currentLocale = Locale.getDefault();
-    String name = resourceClass.getName();
-    ResourceBundle catalog =
-      ResourceBundle.getBundle(name, currentLocale, cl);
-    if (catalog == null) {
-      LOG.warning("Could not get ResourceBundle "+name+" for "+
-                  currentLocale+" from "+cl);
-    }
-    return filter(message, catalog);
-  }
 
-  /** Convert a number of seconds into words
+    /** Convert a number of seconds into words
       @param seconds Number of seconds
       @return Localized words describing the number of seconds.
-  */
-  public static String timeWords(long seconds) {
-    if (seconds == 0) {
-      return filter("0 ${seconds}", Localize.class);
-    }
-    String result = "";
-    long minutes = seconds/60;
-    long hours = minutes/60;
-    long days = hours/24;
-    seconds %= 60;
-    minutes %= 60;
-    hours %= 24;
-    if (days >= 10) {
-      if (hours >=12) ++days;
-      hours = minutes = seconds = 0;
-    } else if (hours >= 10 || days > 0) {
-      if (minutes >=30) {
-        ++hours;
-        days += hours/24;
-        hours %= 24;
-      }
-      minutes = seconds = 0;
-    } else if (minutes >= 10 || hours > 0) {
-      if (seconds >=30) {
-        ++minutes;
-        hours += minutes/60;
+     */
+    public static String timeWords(long seconds) {
+        if (seconds == 0) {
+            return filter("0 ${seconds}", Localize.class);
+        }
+        String result = "";
+        long minutes = seconds/60;
+        long hours = minutes/60;
+        long days = hours/24;
+        seconds %= 60;
         minutes %= 60;
-      }
-      seconds = 0;
-    }
-    if (seconds != 0)
-      result = " " + seconds + " ${second"+ ((seconds>1)?"s}":"}") + result;
-    if (minutes != 0)
-      result = " " + minutes + " ${minute"+ ((minutes>1)?"s}":"}") + result;
-    if (hours != 0)
-      result = " " + hours + " ${hour" + ((hours>1)?"s}":"}") + result;
-    if (days != 0)
-      result = " " + days + " ${day" + ((days>1)?"s}":"}") + result;
+        hours %= 24;
+        if (days >= 10) {
+            if (hours >=12) ++days;
+            hours = minutes = seconds = 0;
+        } else if (hours >= 10 || days > 0) {
+            if (minutes >=30) {
+                ++hours;
+                days += hours/24;
+                hours %= 24;
+            }
+            minutes = seconds = 0;
+        } else if (minutes >= 10 || hours > 0) {
+            if (seconds >=30) {
+                ++minutes;
+                hours += minutes/60;
+                minutes %= 60;
+            }
+            seconds = 0;
+        }
+        if (seconds != 0)
+            result = " " + seconds + " ${second"+ ((seconds>1)?"s}":"}") + result;
+        if (minutes != 0)
+            result = " " + minutes + " ${minute"+ ((minutes>1)?"s}":"}") + result;
+        if (hours != 0)
+            result = " " + hours + " ${hour" + ((hours>1)?"s}":"}") + result;
+        if (days != 0)
+            result = " " + days + " ${day" + ((days>1)?"s}":"}") + result;
 
-    return filter(result.trim(), Localize.class);
-  }
+        return filter(result.trim(), Localize.class);
+    }
 
 }
--- src/test/java/edu/mines/jtk/util/LocalizeTest.java
+++ src/test/java/edu/mines/jtk/util/LocalizeTest.java
@@ -1,124 +1,201 @@
 /****************************************************************************
-Copyright (c) 2003, Landmark Graphics and others. All rights reserved.
-This program and accompanying materials are made available under the terms of
-the Common Public License - v1.0, which accompanies this distribution, and is
-available at http://www.eclipse.org/legal/cpl-v10.html
-****************************************************************************/
+  Copyright (c) 2003, Landmark Graphics and others. All rights reserved.
+  This program and accompanying materials are made available under the terms of
+  the Common Public License - v1.0, which accompanies this distribution, and is
+  available at http://www.eclipse.org/legal/cpl-v10.html
+ ****************************************************************************/
 package edu.mines.jtk.util;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import java.io.IOException;
+import java.util.Locale;
 
 import static edu.mines.jtk.util.Localize.timeWords;
 
 /** Wrap edu.mines.jtk.util.Localize for junit testing.
-   (junit.jar must be in CLASSPATH)
-*/
+  (junit.jar must be in CLASSPATH)
+ */
 public class LocalizeTest extends TestCase {
 
-  /** Junit test code
-     @throws Exception any test failure
-   */
-  public void testAll() throws Exception {
-    {
-      long seconds =(29L + 60*(9));
-      String words = timeWords(seconds);
-      assert (words.equals("9 minutes 29 seconds")) : words;
+    /**
+     * Run some test code. Any "public void test*()" method starting with "test" will be used
+     *
+     */
+    public static void testLocalize() {
+        { // This is the normal way to call it. Gets LocalizeTest.properties, unless we are in France.
+            final Localize dfault = new Localize(LocalizeTest.class);
+            final String sDefault = dfault.format("msg1", 3.14, 42);
+            assert "A number 3.14000 here, and another #42".equals(sDefault) : sDefault;
+        }
+        { // for testing, we specify a specific locale.  Gets LocalizeTest_fr.properties.
+            final Localize fr = new Localize(LocalizeTest.class, null, Locale.FRENCH);
+            final int i = 42;
+            final String sFr = fr.format("msg1", 3.14, i);
+            assert "Un nombre 3.14000 ici, et un autre #42".equals(sFr) : sFr;
+        }
+        { // for testing, we specify an alternate file LocalizeTestAlt[_*].properties
+            final Localize alt = new Localize(LocalizeTest.class, "LocalizeTestAlt");
+            final String s = alt.format("msg1", 3.14, 42);
+            assert "A custom file with number 3.14000, and another #42".equals(s) : s;
+        }
+        { // Specifying format instead of key for format.
+            final Localize alt = new Localize(LocalizeTest.class, "LocalizeTestAlt");
+            final String s = alt.format("No property just a format with number %g.", 3.14);
+            assert "No property just a format with number 3.14000.".equals(s) : s;
+        }
+        { // Specifying non-existent resource file.
+            final Localize alt = new Localize(LocalizeTest.class, "DoesNotExist");
+            final String s = alt.format("A number %g.", 3.14);
+            assert "A number 3.14000.".equals(s) : s;
+        }
+        { // Specifying non-existent resource file.
+            final Localize alt = new Localize(LocalizeTest.class);
+            final String s = alt.format("Ignored number.", 3.14);
+            assert "Ignored number.".equals(s);
+        }
     }
-    {
-      long seconds =(29L + 60*(10));
-      String words = timeWords(seconds);
-      assert (words.equals("10 minutes")) : words;
-    }
-    {
-      long seconds =(30L + 60*(10));
-      String words = timeWords(seconds);
-      assert (words.equals("11 minutes")) : words;
-    }
-    {
-      long seconds =(29L + 60*(29 + 60*(9)));
-      String words = timeWords(seconds);
-      assert (words.equals("9 hours 29 minutes")) : words;
-    }
-    {
-      long seconds =(30L + 60*(30 + 60*(9)));
-      String words = timeWords(seconds);
-      assert (words.equals("9 hours 31 minutes")) : words;
-    }
-    {
-      long seconds =(30L + 60*(30 + 60*(10)));
-      String words = timeWords(seconds);
-      assert (words.equals("11 hours")) : words;
-    }
-    {
-      long seconds =(30L + 60*(30 + 60*(11 +24*9)));
-      String words = timeWords(seconds);
-      assert (words.equals("9 days 12 hours")) : words;
-    }
-    {
-      long seconds =(30L + 60*(30 + 60*(11 +24*10)));
-      String words = timeWords(seconds);
-      assert (words.equals("10 days")) : words;
-    }
-    {
-      long seconds =(0L + 60*(0 + 60*(12 +24*10)));
-      String words = timeWords(seconds);
-      assert (words.equals("11 days")) : words;
-    }
-    {
-      // 2 hours.
-      long seconds = 3600L * 2;
-      String words = timeWords(seconds);
-      assert (words.equals("2 hours")) : words;
-    }
-    {
-      // 1 second less than 2 hours.
-      long seconds = 3600L * 2 - 1;
-      String words = timeWords(seconds);
-      assert (words.equals("2 hours")) : words;
-    }
-    {
-      // 2 days.
-      long seconds = 3600L * 24 * 2;
-      String words = timeWords(seconds);
-      assert (words.equals("2 days")) : words;
-    }
-    {
-      // 1 second less than 2 days.
-      long seconds = 3600L * 24 * 2 - 1;
-      String words = timeWords(seconds);
-      assert (words.equals("2 days")) : words;
-    }
-  }
 
-  // OPTIONAL OPTIONAL OPTIONAL OPTIONAL OPTIONAL OPTIONAL OPTIONAL
+    /**
+     * tests.
+     */
+    public static void testLocale() {
+        assert !Locale.getDefault().equals(Locale.FRANCE)
+                : "The testLocalize unit test does not work in France";
+    }
 
-  /* Initialize objects used by all test methods */
-  @Override protected void setUp() throws Exception { super.setUp();}
+    /**
+     * tests.
+     */
+    public static void testLocalizeThrowable() {
+        final IOException ioException = new IOException("ioe");
+        assert "ioe".equals(Localize.getMessage(ioException));
+        {
+            final IllegalArgumentException e = new IllegalArgumentException(ioException);
+            assert "ioe".equals(Localize.getMessage(e));
+        }
+        {
+            final String better = "Bad argument: " + ioException.getLocalizedMessage();
+            final IllegalArgumentException e = new IllegalArgumentException(better, ioException);
+            assert better.equals(Localize.getMessage(e)): Localize.getMessage(e);
+        }
+        {
+            final IllegalArgumentException e = new IllegalArgumentException(null,ioException);
+            assert "ioe".equals(Localize.getMessage(e));
+        }
+        {
+            final IllegalArgumentException e = new IllegalArgumentException("foo",ioException);
+            assert "foo".equals(Localize.getMessage(e));
+        }
+        {
+            final IllegalArgumentException e = new IllegalArgumentException("foo",null);
+            assert "foo".equals(Localize.getMessage(e));
+        }
+        {
+            final IllegalArgumentException e = new IllegalArgumentException();
+            assert "java.lang.IllegalArgumentException".equals(Localize.getMessage(e)) :Localize.getMessage(e);
+        }
+        {
+            final IllegalArgumentException e = new IllegalArgumentException(null,null);
+            assert "java.lang.IllegalArgumentException".equals(Localize.getMessage(e)) :Localize.getMessage(e);
+        }
+    }
 
-  /* Destruction of stuff used by all tests: rarely necessary */
-  @Override protected void tearDown() throws Exception { super.tearDown();}
+    /** Junit test code
+      @throws Exception any test failure
+     */
+    public static void testLocalizeOld() throws Exception {
+        {
+            long seconds =(29L + 60*(9));
+            String words = timeWords(seconds);
+            assert (words.equals("9 minutes 29 seconds")) : words;
+        }
+        {
+            long seconds =(29L + 60*(10));
+            String words = timeWords(seconds);
+            assert (words.equals("10 minutes")) : words;
+        }
+        {
+            long seconds =(30L + 60*(10));
+            String words = timeWords(seconds);
+            assert (words.equals("11 minutes")) : words;
+        }
+        {
+            long seconds =(29L + 60*(29 + 60*(9)));
+            String words = timeWords(seconds);
+            assert (words.equals("9 hours 29 minutes")) : words;
+        }
+        {
+            long seconds =(30L + 60*(30 + 60*(9)));
+            String words = timeWords(seconds);
+            assert (words.equals("9 hours 31 minutes")) : words;
+        }
+        {
+            long seconds =(30L + 60*(30 + 60*(10)));
+            String words = timeWords(seconds);
+            assert (words.equals("11 hours")) : words;
+        }
+        {
+            long seconds =(30L + 60*(30 + 60*(11 +24*9)));
+            String words = timeWords(seconds);
+            assert (words.equals("9 days 12 hours")) : words;
+        }
+        {
+            long seconds =(30L + 60*(30 + 60*(11 +24*10)));
+            String words = timeWords(seconds);
+            assert (words.equals("10 days")) : words;
+        }
+        {
+            long seconds =(0L + 60*(0 + 60*(12 +24*10)));
+            String words = timeWords(seconds);
+            assert (words.equals("11 days")) : words;
+        }
+        {
+            // 2 hours.
+            long seconds = 3600L * 2;
+            String words = timeWords(seconds);
+            assert (words.equals("2 hours")) : words;
+        }
+        {
+            // 1 second less than 2 hours.
+            long seconds = 3600L * 2 - 1;
+            String words = timeWords(seconds);
+            assert (words.equals("2 hours")) : words;
+        }
+        {
+            // 2 days.
+            long seconds = 3600L * 24 * 2;
+            String words = timeWords(seconds);
+            assert (words.equals("2 days")) : words;
+        }
+        {
+            // 1 second less than 2 days.
+            long seconds = 3600L * 24 * 2 - 1;
+            String words = timeWords(seconds);
+            assert (words.equals("2 days")) : words;
+        }
+    }
 
-  // NO NEED TO CHANGE THE FOLLOWING
+    // NO NEED TO CHANGE THE FOLLOWING
 
-  /** Standard constructor calls TestCase(name) constructor
+    /** Standard constructor calls TestCase(name) constructor
       @param name Name of junit Test.
-   */
-  public LocalizeTest(String name) {super (name);}
+     */
+    public LocalizeTest(String name) {super (name);}
 
-  /** This automatically generates a suite of all "test" methods.
+    /** This automatically generates a suite of all "test" methods.
       @return A suite of all junit tests as a Test.
-   */
-  public static junit.framework.Test suite() {
-    try {assert false; throw new IllegalStateException("need -ea");}
-    catch (AssertionError e) {}
-    return new TestSuite(LocalizeTest.class);
-  }
+     */
+    public static junit.framework.Test suite() {
+        try {assert false; throw new IllegalStateException("need -ea");}
+        catch (AssertionError e) {}
+        return new TestSuite(LocalizeTest.class);
+    }
 
-  /** Run all tests with text gui if this class main is invoked
+    /** Run all tests with text gui if this class main is invoked
       @param args Command-line arguments.
-   */
-  public static void main (String[] args) {
-    junit.textui.TestRunner.run (suite());
-  }
+     */
+    public static void main (String[] args) {
+        junit.textui.TestRunner.run (suite());
+    }
 }
