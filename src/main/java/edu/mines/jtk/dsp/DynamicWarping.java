7760bed4b845139524e6c4de54f66d41c8192159
Dave Hale <dhale@mines.edu>
2012-10-02 09:45:51 -0600

    added copyright

--- src/main/java/edu/mines/jtk/dsp/DynamicWarping.java
+++ src/main/java/edu/mines/jtk/dsp/DynamicWarping.java
@@ -1,3 +1,9 @@
+/****************************************************************************
+Copyright (c) 2012, Colorado School of Mines and others. All rights reserved.
+This program and accompanying materials are made available under the terms of
+the Common Public License - v1.0, which accompanies this distribution, and is
+available at http://www.eclipse.org/legal/cpl-v10.html
+****************************************************************************/
 package edu.mines.jtk.dsp;
 
 import edu.mines.jtk.util.*;
